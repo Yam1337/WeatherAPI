21643acf375e8d51f242447ef39dffed30d3bd04
Rafal Medryk <yam1337@gmail.com>
2021-03-04 18:11:33 +0100

    bugfixes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var srcBtn = document.getElementById("search2")
 var test = icon.src.innerHTML
 var OnButtonClick = function() {
     var city1 = document.getElementById("search1").value;
-    fetch("http://api.weatherapi.com/v1/current.json?key=55cbdff15f4f49aaa9f132150210303&q=" + city1)
+    fetch("https://api.weatherapi.com/v1/current.json?key=55cbdff15f4f49aaa9f132150210303&q=" + city1)
     .then(res => res.json())
     .then(data => {
         icon.src = "http:" + data.current.condition.icon
